324f79423dad3eb066f28b25ef8c8f2056dcc2ac
Viral Patel <viral.y.patel@gmail.com>
2016-08-03 00:11:41 +0530

    search screen ui additions - criteria and mailbox type

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+// refer this for toggle options - http://codepen.io/8odoros/pen/BzxAwm
+
+function onLoadSearchPage(){
+    var criterias = ["#sTo", "#sFrom", "#sSubject"];
+    for(var i = 0; i < criterias.length; i++){
+        $( criterias[i] ).toggle("slow");
+    }
+
+}
+
+function toggleSection(divName){
+  $( divName ).toggle("slow");
+}
 
